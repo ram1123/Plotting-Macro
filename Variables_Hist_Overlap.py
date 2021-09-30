92c92a653e39a497f8e2e8893f1e2bfe0530aaa0
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2021-09-30 05:16:09 +0200

    Updated variable list

--- Variables_Hist_Overlap.py
+++ Variables_Hist_Overlap.py
@@ -1,16 +1,6 @@
 twoD_var_list = [
     [
-        "New_pTBasedSel_W1_mass", 100, 0.0, 200.0 ,r'New_pTBasedSel_W1_mass',
-        "SumTwoMaxBjets_Jet_mass", 100, 0, 200, r'SumTwoMaxBjets_Jet_mass',
-        "MaxBScore_MaxPt_Jet_mass", 100, 0.0, 200.0, r'MaxBScore_MaxPt_Jet_mass',
-        "New_OnShellW_Mass", 100, 0.0, 200.0, r'New_OnShellW_Mass',
-        "New_OffShellW_Mass", 100, 0.0, 200.0, r'New_OffShellW_Mass'
-    ],
-    [
-        "New_pTBasedSel_W2_mass", 100, 0.0, 200.0 ,r'New_pTBasedSel_W2_mass',
-        # "SumTwoMaxBjets_Jet_mass", 100, 0, 200, r'SumTwoMaxBjets_Jet_mass',
-        # "MaxBScore_MaxPt_Jet_mass", 100, 0.0, 200.0, r'MaxBScore_MaxPt_Jet_mass',
-        "New_OnShellW_Mass", 100, 0.0, 200.0, r'New_OnShellW_Mass',
-        "New_OffShellW_Mass", 100, 0.0, 200.0, r'New_OffShellW_Mass'
+        "FullyResolved_TwoLeadingJets_m", 100, 0.0, 1000.0 ,r'FullyResolved_TwoLeadingJets_m',
+        "FullyResolved_ThirdFourthJets_m", 100, 0, 700, r'FullyResolved_ThirdFourthJets_m'
     ]
 ]
--- commands.sh
+++ commands.sh
@@ -3,46 +3,12 @@
 # @Last Modified by:   Ram Krishna Sharma
 # @Last Modified time: 2021-06-30
 
-## resonant commands
+# 1D plot: compare same branch from multiple root file
 python ReadTree_CompTwoRootFile_Mask.py  -i GluGluToRadionToHHTo2G4Q_M250.root,GluGluToRadionToHHTo2G4Q_M1000.root,GluGluToRadionToHHTo2G4Q_M3000.root -t Events,Events,Events -l M250,M1000,M3000 -DataPosition 5 --dir pltos
 
 
-## Non-resonant commands
+# 1D plot: Compare different branches from same root file
+python ReadTree_HistOverlap.py -var_file Variables_Hist_Overlap.py -i GluGluToRadionToHHTo2G4Q_M250.root -t Events --dir plots_HistOverlap
 
-python ReadTree_CompTwoRootFile.py -i /afs/cern.ch/user/r/rasharma/work/doubleHiggs/flashgg/ScaleWWZZ/CMSSW_10_6_8/src/WWZZ_Signal_Addition/GluGluToHHTo2G4Q_node_cHHH1_2017.root,/afs/cern.ch/user/r/rasharma/work/doubleHiggs/flashgg/ScaleWWZZ/CMSSW_10_6_8/src/WWZZ_Signal_Addition/GluGluToHHTo2B2G_node_cHHH1_2017.root,/afs/cern.ch/user/r/rasharma/work/doubleHiggs/flashgg/ScaleWWZZ/CMSSW_10_6_8/src/WWZZ_Signal_Addition/GluGluToHHTo2G2ZTo2G4Q_node_cHHH1_2017.root -t tagsDumper/trees/GluGluToHHTo2G4Q_node_cHHH1_13TeV_HHWWggTag_1,tagsDumper/trees/GluGluToHHTo2B2G_node_cHHH1_13TeV_HHWWggTag_1,tagsDumper/trees/GluGluToHHTo2G2ZTo2G4Q_node_cHHH1_13TeV_HHWWggTag_1 -l WWgg,bbgg,ZZgg --dir MorVar
-
-time(python ReadTree_CompTwoRootFile.py -i /afs/cern.ch/user/r/rasharma/work/doubleHiggs/flashgg/ScaleWWZZ/CMSSW_10_6_8/src/WWZZ_Signal_Addition/GluGluToHHTo2G4Q_node_cHHH1_2017.root,/afs/cern.ch/user/r/rasharma/work/doubleHiggs/flashgg/ScaleWWZZ/CMSSW_10_6_8/src/WWZZ_Signal_Addition/GluGluToHHTo2B2G_node_cHHH1_2017.root,/afs/cern.ch/user/r/rasharma/work/doubleHiggs/flashgg/ScaleWWZZ/CMSSW_10_6_8/src/WWZZ_Signal_Addition/GluGluToHHTo2G2ZTo2G4Q_node_cHHH1_2017.root -t tagsDumper/trees/GluGluToHHTo2G4Q_node_cHHH1_13TeV_HHWWggTag_1,tagsDumper/trees/GluGluToHHTo2B2G_node_cHHH1_13TeV_HHWWggTag_1,tagsDumper/trees/GluGluToHHTo2G2ZTo2G4Q_node_cHHH1_13TeV_HHWWggTag_1 -l WWgg,bbgg,ZZgg --dir MorVar)
-
-#With data
-time(python ReadTree_CompTwoRootFile_Mask.py -i /afs/cern.ch/user/r/rasharma/work/doubleHiggs/flashgg/ScaleWWZZ/CMSSW_10_6_8/src/WWZZ_Signal_Addition/GluGluToHHTo2G4Q_node_cHHH1_2017.root,/afs/cern.ch/user/r/rasharma/work/doubleHiggs/flashgg/ScaleWWZZ/CMSSW_10_6_8/src/WWZZ_Signal_Addition/GluGluToHHTo2B2G_node_cHHH1_2017.root,/afs/cern.ch/user/r/rasharma/work/doubleHiggs/flashgg/ScaleWWZZ/CMSSW_10_6_8/src/WWZZ_Signal_Addition/GluGluToHHTo2G2ZTo2G4Q_node_cHHH1_2017.root,/afs/cern.ch/user/r/rasharma/work/doubleHiggs/flashgg/ScaleWWZZ/CMSSW_10_6_8/src/WWZZ_Signal_Addition/Data_2017.root -t tagsDumper/trees/GluGluToHHTo2G4Q_node_cHHH1_13TeV_HHWWggTag_1,tagsDumper/trees/GluGluToHHTo2B2G_node_cHHH1_13TeV_HHWWggTag_1,tagsDumper/trees/GluGluToHHTo2G2ZTo2G4Q_node_cHHH1_13TeV_HHWWggTag_1,tagsDumper/trees/Data_13TeV_HHWWggTag_1 -l WWgg,bbgg,ZZgg,DataSB --dir After_BScoreBugFix -DataPosition 3)
-
-
-python ReadTree_HistOverlap.py -i /afs/cern.ch/user/r/rasharma/work/doubleHiggs/flashgg/ScaleWWZZ/CMSSW_10_6_8/src/WWZZ_Signal_Addition/GluGluToHHTo2G4Q_node_cHHH1_2017.root --dir CheckDiffMass -logY 1
-python ReadTree_HistOverlap.py -i /afs/cern.ch/user/r/rasharma/work/doubleHiggs/flashgg/ScaleWWZZ/CMSSW_10_6_8/src/WWZZ_Signal_Addition/GluGluToHHTo2B2G_node_cHHH1_2017.root --dir CheckDiffMass -logY 1 -t tagsDumper/trees/GluGluToHHTo2B2G_node_cHHH1_13TeV_HHWWggTag_1
-
-
-##### old
-python ReadTree_2D.py -i /afs/cern.ch/user/r/rasharma/work/h4L_analysis/UFHZZAnalyzer/CMSSW_10_6_12/src/GluGluHToZZTo4L_M125_fy_MoreStat.root --dir RapidityWeighted_functionOnly_RedsY20 -zaxis 20 &
-python ReadTree_2D.py -i /afs/cern.ch/user/r/rasharma/work/h4L_analysis/UFHZZAnalyzer/CMSSW_10_6_12/src/GluGluHToZZTo4L_M125_fy_pTWeighted_MoreStat.root --dir RapidityWeighted_pTWeighted_RedsY20 -zaxis 20  -var_file Variables_TwoDPlots_pTWeighted.py&
-python ReadTree_2D.py -i /afs/cern.ch/user/r/rasharma/work/h4L_analysis/UFHZZAnalyzer/CMSSW_10_6_12/src/GluGluHToZZTo4L_M125_fy_pTWeighted_UsingEnergy_v2_MoreStat.root --dir RapidityWeighted_pTWeightedUsingEnergyV2_RedsY20_MoreStat -zaxis 20  -var_file Variables_TwoDPlots_EnergyWeighted.py&
-
-
-
-python ReadTree.py -i /afs/cern.ch/user/r/rasharma/work/h4L_analysis/UFHZZAnalyzer/CMSSW_10_6_12/src/GluGluHToZZTo4L_M125_fy_MoreStat.root --dir RapidityWeighted_functionOnly_1D_RedsY20  -var_file Variables_Simple1DPlots_functionOnlly.py &
-python ReadTree.py -i /afs/cern.ch/user/r/rasharma/work/h4L_analysis/UFHZZAnalyzer/CMSSW_10_6_12/src/GluGluHToZZTo4L_M125_fy_pTWeighted_MoreStat.root --dir RapidityWeighted_pTWeighted_1D_RedsY20 &
-python ReadTree.py -i /afs/cern.ch/user/r/rasharma/work/h4L_analysis/UFHZZAnalyzer/CMSSW_10_6_12/src/GluGluHToZZTo4L_M125_fy_pTWeighted_UsingEnergy_v2_MoreStat.root --dir RapidityWeighted_pTWeightedUsingEnergyV2_1D_RedsY20 &
-python ReadTree.py -i /afs/cern.ch/user/r/rasharma/work/h4L_analysis/UFHZZAnalyzer/CMSSW_10_6_12/src/GluGluHToZZTo4L_M125_fy_pTWeighted_UsingEnergy_v2_MoreStat.root --dir RapidityWeighted_pTWeightedUsingEnergyV2_1D_Rapidity_RedsY20 -v nJettiness_Rapidity&
-python ReadTree.py -i /afs/cern.ch/user/r/rasharma/work/h4L_analysis/UFHZZAnalyzer/CMSSW_10_6_12/src/GluGluHToZZTo4L_M125_fy_pTWeighted_MoreStat.root --dir RapidityWeighted_pTWeighted_1D_Rapidity_RedsY20 -v nJettiness_Rapidity&
-python ReadTree.py -i /afs/cern.ch/user/r/rasharma/work/h4L_analysis/UFHZZAnalyzer/CMSSW_10_6_12/src/GluGluHToZZTo4L_M125_fy_MoreStat.root --dir RapidityWeighted_functionOnly_1D_Rapidity_RedsY20 -v nJettiness_Rapidity&
-
-
-python ReadTree_HistOverlap.py -i /afs/cern.ch/user/r/rasharma/work/h4L_analysis/UFHZZAnalyzer/CMSSW_10_6_12/src/GluGluHToZZTo4L_M125_fy_MoreStat.root --dir  RapidityWeighted_functionOnly_HistComp -var_file Variables_Hist_Overlap_function.py &
-python ReadTree_HistOverlap.py -i /afs/cern.ch/user/r/rasharma/work/h4L_analysis/UFHZZAnalyzer/CMSSW_10_6_12/src/GluGluHToZZTo4L_M125_fy_pTWeighted_UsingEnergy_v2_MoreStat.root --dir  RapidityWeighted_pTWeightedUsingEnergy_HistComp -var_file Variables_Hist_Overlap.py &
-
-
-# Generalised Tau
-#
-
-python ReadTree.py -i /afs/cern.ch/user/r/rasharma/work/h4L_analysis/UFHZZAnalyzer/CMSSW_10_6_12/src/GluGluHToZZTo4L_M125_AllTaus.root --dir NJettiness_1D &
-python ReadTree_HistOverlap.py -i /afs/cern.ch/user/r/rasharma/work/h4L_analysis/UFHZZAnalyzer/CMSSW_10_6_12/src/GluGluHToZZTo4L_M125_AllTaus.root --dir NJettiness_Comparison &
-
+# 2D plot
+python ReadTree_2D.py -i GluGluToRadionToHHTo2G4Q_M250.root -t Events --dir plots_2D -var_file Variables_TwoDPlots.py
